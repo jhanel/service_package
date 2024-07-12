aa15f4c154eefe0d3c8355efba04e222365fd5e9
jhanel <72320010+jhanel@users.noreply.github.com>
2024-07-12 14:35:59 -0400

    Tracker timeline
    Implemented timeline tile in track_order.dart to update order status

--- project/lib/track_order.dart
+++ project/lib/track_order.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:timeline_tile/timeline_tile.dart';
 
 class TrackOrderPage extends StatefulWidget {
   const TrackOrderPage({super.key});
 
   @override
-  _TrackOrderPageState createState() => _TrackOrderPageState();
+  TrackOrderPageState createState() => TrackOrderPageState();
 }
 
-class _TrackOrderPageState extends State<TrackOrderPage> {
+class TrackOrderPageState extends State<TrackOrderPage> {
   final TextEditingController _orderIdController = TextEditingController();
   String _orderStatus = '';
 
   void _trackOrder() {
     // Simulate an order tracking process
     setState(() {
-      _orderStatus = 'Order #${_orderIdController.text} is on its way!';
+      _orderStatus = 'Results for Order #${_orderIdController.text}:';
     });
   }
 
@@ -45,8 +46,46 @@ class _TrackOrderPageState extends State<TrackOrderPage> {
               _orderStatus,
               style: const TextStyle(fontSize: 18.0),
             ),
+            const SizedBox(height: 16.0),
+            if (_orderStatus.isNotEmpty) ...[
+              _buildTimelineTile('Order Received', Icons.shopping_cart, true),
+              _buildTimelineTile('Order Reviewed', Icons.local_shipping, false),
+              _buildTimelineTile('Order Shipped', Icons.local_shipping, false),
+              _buildTimelineTile('Out for Delivery', Icons.directions_bike, false),
+              _buildTimelineTile('Delivered', Icons.home, false),
+            ],
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildTimelineTile(String title, IconData icon, bool isCompleted) {
+    return TimelineTile(
+      alignment: TimelineAlign.start,
+      indicatorStyle: IndicatorStyle(
+        width: 40,
+        height: 40,
+        indicator: Container(
+          decoration: BoxDecoration(
+            color: isCompleted ? Colors.green : Colors.grey,
+            shape: BoxShape.circle,
+          ),
+          child: Icon(icon, color: Colors.white),
+        ),
+      ),
+      endChild: Container(
+        alignment: Alignment.centerLeft,
+        padding: const EdgeInsets.all(8.0),
+        child: Text(title, style: const TextStyle(fontSize: 18.0)),
+      ),
+      beforeLineStyle: LineStyle(
+        color: isCompleted ? Colors.green : Colors.grey,
+        thickness: 4,
+      ),
+      afterLineStyle: LineStyle(
+        color: isCompleted ? Colors.green : Colors.grey,
+        thickness: 4,
       ),
     );
   }
